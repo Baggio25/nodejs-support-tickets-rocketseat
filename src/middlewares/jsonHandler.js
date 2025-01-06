e0c5c6b923a5bd4a3f6c7cf49f2a10b1f6402fae
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-01-05 22:45:41 -0300

    Parâmetros dinâmicos

--- src/middlewares/jsonHandler.js
+++ src/middlewares/jsonHandler.js
@@ -1,16 +1,17 @@
+
 export async function jsonHandler(request, response) {
   const buffers = [];
 
   for await (const chunk of request) {
-    buffers.push(chunk)
+    buffers.push(chunk);
   }
 
   try {
     request.body = JSON.parse(Buffer.concat(buffers).toString());
+    console.log(request.body);
   } catch (error) {
     request.body = null;
     console.log(error);
-    
   }
 
   response.setHeader("Content-Type", "application/json");
---
+++ src/utils/parseRoutePath.js
@@ -0,0 +1,13 @@
+
+
+export function parseRoutePath(path) {
+  const routeParametersRegex = /:([a-zA-Z]+)/g;
+
+  const params = path.replaceAll(routeParametersRegex,
+    "(?<$1>[a-z0-9-_]+)"
+  );
+
+  const pathRegex = new RegExp(`ˆ${params}(?<query>\\?(.*))?$`);
+
+  return pathRegex;
+} 
